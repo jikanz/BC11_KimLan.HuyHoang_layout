7a930023819ee57dfbe62731bddd33123f143f2f
jikanz <kl971995@gmail.com>
2021-06-13 20:31:19 +0700

    moi

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
-var btn3 = document.getElementById("myBtn3");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+mybutton = document.getElementById("myBtn");
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-btn2.onclick = function () {
-  modal.style.display = "block";
-};
-btn3.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 window.onscroll = function () {
-  myFunction();
+  scrollFunction();
 };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    navbar.classList.remove("sticky");
+    mybutton.style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
